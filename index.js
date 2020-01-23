4b20507d2f5388c9bb74adac8d260fd28b6f40f6
schwadan001 <schwadan001@gmail.com>
2020-01-22 22:15:54 -0600

    add static web ui

---
+++ index.js
@@ -0,0 +1,21 @@
+const othello = new Othello();
+const board = new OthelloBoard(othello, {
+    "displayMoves": "b",
+    //"onBlackMove": executeAiMove,
+    "onWhiteMove": executeAiMove,
+});
+
+function executeAiMove() {
+    setTimeout(aiMove, 1000);
+}
+
+function shuffle(arr) {
+    var j, x, i;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+    return arr;
+}
